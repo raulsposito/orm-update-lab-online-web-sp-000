5e39ab2ef8e9c789c477ce1024f1dbce37f3d17e
Raul Sposito <raulsposito@gmail.com>
2020-07-07 23:58:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,4 +29,20 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save 
+    if self.id 
+      self.update 
+    else 
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_inserted_rowid() FROM students")[0][0]
+    end
+    
+  end
+
 end
