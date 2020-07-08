d62f626c7943419be54adcc00009e3aa18ba7727
Raul Sposito <raulsposito@gmail.com>
2020-07-08 00:12:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,11 +58,11 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL 
+    sql = <<-SQL
       SELECT *
-      FROM students 
-      WHERE name = ? 
-      LIMIT 1 
+      FROM students
+      WHERE name = ?
+      LIMIT 1
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
