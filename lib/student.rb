3843186a498a39efb0f1908552a4e54a848d219b
Raul Sposito <raulsposito@gmail.com>
2020-07-08 00:14:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,4 +70,9 @@ class Student
     end.first
   end
 
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
 end
